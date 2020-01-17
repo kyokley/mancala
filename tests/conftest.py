53fad65953f33b4f77e1b68898f1ff7800cac8fd
Kevin Yokley <kyokley2@gmail.com>
2020-01-16 21:35:34 -0600

    Add test factories

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+from pytest_factoryboy import register
+from tests.factories import PlayerFactory
+
+
+register(PlayerFactory)
---
+++ tests/factories.py
@@ -0,0 +1,15 @@
+import factory
+
+from faker import Factory as FakerFactory
+from src.player import Player
+
+
+faker = FakerFactory.create()
+
+
+class PlayerFactory(factory.Factory):
+    name = factory.LazyAttribute(lambda x: faker.name())
+    color = 'normal'
+
+    class Meta:
+        model = Player
