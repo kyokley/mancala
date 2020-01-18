d6a1427dfe78fbed1889a28dc5c7cbea145e29c0
Kevin Yokley <kyokley2@gmail.com>
2020-01-18 09:59:15 -0600

    Add tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -3,7 +3,7 @@ import pytest
 from src.player import DefensivePlayer
 
 
-class TestLegalMovesInReverse:
+class TestDefensiveMove:
     @pytest.fixture(autouse=True)
     def setUp(self, basic_game_setup):
         self.player = DefensivePlayer('Player1')
@@ -11,7 +11,7 @@ class TestLegalMovesInReverse:
 
         self.board.initialize_cups(0)
 
-    def test_move(self):
+    def test_move1(self):
         """
               a   b   c   d   e   f
               0   3   3   0   1   1
@@ -28,3 +28,21 @@ class TestLegalMovesInReverse:
 
         move = self.player._defensive_move()
         assert move == 'e'
+
+    def test_move2(self):
+        """
+              a   b   c   d   e   f
+              0   2   4   3   1   1
+          0                           0
+              0   0   0   0   0   0
+              g   h   i   j   k   l
+        """
+        self.board.cups[1] = 0
+        self.board.cups[2] = 2
+        self.board.cups[3] = 4
+        self.board.cups[4] = 3
+        self.board.cups[5] = 1
+        self.board.cups[6] = 1
+
+        move = self.player._defensive_move()
+        assert move == 'b'
